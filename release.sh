6e426771b14c3eb399ec3a366d21ff982958def8
Dustin Jacobsen <duja9708@colorado.edu>
2024-06-25 21:45:20 +0100

    git text attributes

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for [ envar in ('HOST', 'PORT', 'JAWSDB_URL', 'STATSD_HOST', 'STATSD_PORT', 'HOSTEDGRAPHITE_APIKEY', 'CLOUDAMQP_URL')]; do
+for envar in 'HOST', 'PORT', 'JAWSDB_URL', 'STATSD_HOST', 'STATSD_PORT', 'HOSTEDGRAPHITE_APIKEY', 'CLOUDAMQP_URL'; do
 	if [ -z "$$envar" ] ; then
 		echo "Fatal: missing mandatory environment variable $envar" 1>&2
 		exit -1
